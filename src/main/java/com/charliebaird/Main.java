b15c072387f25b4bf51602862b1353f977ebc41c
Charlie Baird <charliembaird@gmail.com>
2025-04-08 16:58:01 -0700

    Add MapRunner class

--- src/main/java/com/charliebaird/Main.java
+++ src/main/java/com/charliebaird/Main.java
@@ -1,10 +1,14 @@
 package com.charliebaird;
 
+import com.charliebaird.Minimap.Legend;
+import com.charliebaird.Minimap.MapRunner;
 import com.charliebaird.Minimap.MinimapExtractor;
 import com.charliebaird.PoEBot.PoEBot;
+import com.charliebaird.teensybottinglib.InputCodes.MouseCode;
 import com.charliebaird.utility.ScreenCapture;
 import com.charliebaird.utility.Timer;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class Main
@@ -15,67 +19,51 @@ public class Main
 
     public static void main(String[] args)
     {
-        PoEBot teensyBot = new PoEBot();
-
-            if (args.length != 1)
-            {
-                String imagePath = "C:/Users/charl/Documents/dev/CB/PoE/MinimapReader/samples/minimap1.png";
-                Mat original = Imgcodecs.imread(imagePath);
-
-                Timer.start();
-                MinimapExtractor minimap = new MinimapExtractor(writeToDisk);
-                minimap.resolve(original);
-                Timer.stop();
-                minimap.saveFinalMinimap("final.png");
-            }
-
-            else if (args[0].equals("-l"))
-            {
-                Timer.start();
-                Mat original = ScreenCapture.captureScreenMat();
-                Timer.stop();
-
-                Timer.start();
-                MinimapExtractor minimap = new MinimapExtractor(writeToDisk);
-                minimap.resolve(original);
-                Timer.stop();
-                minimap.saveFinalMinimap("final.png");
-
-//                Point p = minimap.findOptimalRevealAngle();
-//                if (p != null)
-//                {
-//                    Point screenPoint = Legend.convertMinimapPointToScreen(p);
-
-//                    robot.mouseMoveGeneralLocation(screenPoint);
-//                }
-
-//                minimap.saveFinalMinimap("final.png");
-            }
+        if (args.length != 1)
+        {
+//            String imagePath = "C:/Users/charl/Documents/dev/CB/PoE/MinimapReader/samples/minimap1.png";
+//            Mat original = Imgcodecs.imread(imagePath);
 //
-//            else if (args[0].equals("-r"))
+//            Timer.start();
+//            MinimapExtractor minimap = new MinimapExtractor(writeToDisk);
+//            minimap.resolve(original);
+//            Timer.stop();
+//            minimap.saveFinalMinimap("final.png");
+        }
+
+        else if (args[0].equals("-l"))
+        {
+//            Timer.start();
+//            Mat original = ScreenCapture.captureScreenMat();
+//            Timer.stop();
+//
+//            Timer.start();
+//            MinimapExtractor minimap = new MinimapExtractor(writeToDisk);
+//            minimap.resolve(original);
+//            Timer.stop();
+//            minimap.saveFinalMinimap("final.png");
+//
+//            Point p = minimap.findOptimalRevealAngle(0);
+//            if (p != null)
 //            {
-//                for (int i=0; i<30; i++)
-//                {
-//                    Timer.start();
-//                    Mat original = getScreenshot();
+//                Point screenPoint = Legend.convertMinimapPointToScreen(p);
 //
-//                    MinimapExtractor minimap = new MinimapExtractor(writeToDisk);
-//                    minimap.resolve(original);
-//                    robot.moveCharacter(minimap);
-//                    robot.holdRightClick();
-//                    Timer.stop();
-//
-//
-////                    if (p != null)
-////                    {
-//
-////                        robot.moveCharacter(minimap);
-//
-//
-////                    }
-//                }
-//                robot.releaseRightClick();
+//                bot.mouseMoveGeneralLocation(screenPoint);
 //            }
+//
+//            minimap.saveFinalMinimap("final.png");
+        }
+
+        else if (args[0].equals("-r"))
+        {
+            MapRunner runner = new MapRunner();
+            for (int i=0; i<25; i++)
+            {
+                runner.runIteration();
+            }
+
+            runner.exitMap();
+        }
     }
 
 }
